53f9501cce1baabe854667280b80aac15d02ab18
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2022-12-08 16:01:08 +0000

    add home page + pathway to load when entering page

--- bookboba/urls.py
+++ bookboba/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from book_bobatea.views import get_home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', get_home_page, name='home_page')
 ]
