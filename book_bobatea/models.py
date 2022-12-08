798b200530c12039b5c45a2c26bcc259b3733e56
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2022-12-08 15:33:43 +0000

    add slug field to tea menu

--- book_bobatea/models.py
+++ book_bobatea/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -8,7 +9,17 @@ class TeaMenu(models.Model):
     description = models.TextField(max_length=500)
     quantity = models.IntegerField()
     price = models.IntegerField()
-
+    slug = models.SlugField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+    
+    def save(self, *args, **kwargs):
+        if not self.slug and self.name:
+            self.slug = slugify(self.name)
+        super(TeaMenu , self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Tea Menu'
+        verbose_name_plural = 'Tea Menus'
