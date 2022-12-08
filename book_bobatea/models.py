e76bf23046275c61e443f95bfc434f9825c75c2f
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2022-12-08 15:06:54 +0000

    add teamenu model to models

--- book_bobatea/models.py
+++ book_bobatea/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TeaMenu(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    quantity = models.IntegerField()
+    price = models.IntegerField()
