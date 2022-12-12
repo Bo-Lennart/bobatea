bd26d657375fa325e7076569967af2a08fd103fa
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2022-12-12 14:21:59 +0000

    add reservation edit page form

--- book_bobatea/views.py
+++ book_bobatea/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import TeaMenu, AboutUs, StaffCrew, Reservation
 from .forms import ReserveTableForm, add_item_to_menu_form
 
@@ -10,20 +10,6 @@ def get_menu(request):
     context = {'menu': menu}
 
     return render(request, '../templates/base.html', context)
-
-
-def add_menu_item(request):
-    add_item = add_item_to_menu_form()
-
-    if request.method == 'POST':
-        add_item = add_item_to_menu_form(request.POST)
-        if add_item.is_valid():
-            add_item.save()
-        return redirect(staff_page)
-
-    context = {'add_item': add_item}
-
-    return render(request, '../templates/add_menu_item.html', context)
 
 
 def staff_reserve_table(request):
@@ -82,10 +68,35 @@ def staff_page(request):
     return render(request, '../templates/staff_page.html', context)
 
 
+def add_menu_item(request):
+    add_item = add_item_to_menu_form()
+
+    if request.method == 'POST':
+        add_item = add_item_to_menu_form(request.POST)
+        if add_item.is_valid():
+            add_item.save()
+        return redirect(staff_page)
+
+    context = {'add_item': add_item}
+
+    return render(request, '../templates/add_menu_item.html', context)
+
+
 def edit_menu(request, TeaMenu_id):
-    return render(request, '../templates/edit_menu.html')
+    item = get_object_or_404(TeaMenu, id=TeaMenu_id)
+    form = add_item_to_menu_form(instance=item)
+    context = {
+        'form': form
+    }
+
+    return render(request, '../templates/edit_menu.html', context)
 
 
 def edit_reservation(request, Reservation_id):
-    return render(request, '../templates/edit_reservation.html')
-    
+    booking = get_object_or_404(Reservation, id=Reservation_id)
+    form = ReserveTableForm(instance=booking)
+    context = {
+        'form': form
+    }
+
+    return render(request, '../templates/edit_reservation.html', context)
