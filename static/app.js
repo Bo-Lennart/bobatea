63ebfaddb9fc378d52c3941ed5951bce75a8aff8
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2022-12-15 11:40:58 +0000

    add thank you message when submission of reservation has been made

---
+++ static/app.js
@@ -0,0 +1,10 @@
+var nextStep = document.querySelector('#submit_reservation');
+          
+            nextStep.addEventListener('click', function (e) {
+              e.preventDefault();
+              // Hide reservation form
+              document.getElementById('reservation_form').style.display = 'none';
+          
+              // Show thank you message
+              document.getElementById('thank_you').style.display = 'block';
+            });
