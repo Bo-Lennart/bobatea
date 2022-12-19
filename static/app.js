799a047c5a5a4bcfb67c777c135a6d8fd2c80c4d
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2022-12-19 16:37:14 +0000

    add styling to about page

--- static/app.js
+++ static/app.js
@@ -3,36 +3,53 @@ const navMenu = document.querySelector(".nav_menu");
 
 var today = new Date();
 var dd = today.getDate();
-var mm = today.getMonth()+1;
+var mm = today.getMonth() + 1;
 var yyyy = today.getFullYear();
 var todaysDate = new Date().getDate
-
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
 })
 
-document.querySelectorAll('.nav-link').forEach( n=> n.
-    addEventListener('click', () => {
-        hamburger.classList.remove("active");
-        navMenu.classList.remove("active");
-    }))
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
 
 
-function deleteReservation() {
+function deleteCancelationAlert() {
+    alert("Cancelation request has been deleted");
+}
+
+function deleteReservationAlert() {
     alert("Reservation has been deleted");
 }
 
-if(dd<10){
-    dd='0'+dd
-} 
-if(mm<10){
-    mm='0'+mm
-} 
+function deleteItemMenuAlert() {
+    alert("Menu item has been deleted");
+}
 
-today = yyyy+'-'+mm+'-'+dd;
+function itemEditedAlert() {
+    alert("Menu has been updated");
+}
 
-document.getElementById("id_date").setAttribute("min", today, "max", 'nextWeek');
+function reservationEditedAlert() {
+    alert("Reservation has been updated");
+}
+
+today = yyyy + '-' + mm + '-' + dd;
 
 
+if (dd < 10) {
+    dd = '0' + dd
+}
+if (mm < 10) {
+    mm = '0' + mm
+}
+
+var calendarElement = document.getElementById("id_date")
+
+if (calendarElement !== null) {
+    calendarElement.setAttribute("min", today, "max", 'nextWeek');
+}
